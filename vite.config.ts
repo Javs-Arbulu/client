9c28f2255568ac305c0fac48ce3c0e072b0cc27b
Javs-Arbulu <jaarbuluc@hotmail.com>
2025-07-20 05:05:38 -0500

    Uptade vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ['iglesianuevacasa.online'],
-    host: true,
-    port: 4200,
-    strictPort: true,
-    watch: {
-      usePolling: true,
-      interval: 100,
-    },
-    hmr: {
-      host: "localhost",
-      protocol: "ws",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: [env.VITE_ALLOWED_HOST],
+      host: true,
       port: 4200,
+      strictPort: true,
+      watch: {
+        usePolling: true,
+        interval: 100,
+      },
+      hmr: {
+        host: env.VITE_HOST,
+        protocol: "ws",
+        port: 4200,
+      },
     },
-  },
+  };
 });
