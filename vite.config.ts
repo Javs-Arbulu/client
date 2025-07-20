cc76b76c04812c01ffaa34b54468984651b9d597
Javs-Arbulu <jaarbuluc@hotmail.com>
2025-07-20 03:54:08 -0500

    Update Vite server host and allowedHosts config
    Replaces the server host value with 'true' and adds 'iglesianuevacasa.online' to allowedHosts. This change improves server accessibility and restricts allowed hosts for security.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    allowedHosts: ['iglesianuevacasa.online'],
+    host: true,
     port: 4200,
     strictPort: true,
     watch: {
